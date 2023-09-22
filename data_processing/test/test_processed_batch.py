95e764fe3ab50a2c105b8f03fc3cd2c48c366cd9
Chris Lacina <clacina@gmail.com>
2023-09-22 12:10:34 -0700

    Unused files?

--- data_processing/test/test_processed_batch.py
+++
@@ -1,6 +0,0 @@
-from data_processing.db_utils import fetch_processed_transactions_from_batch
-
-
-def test_processed_batch_retrieval():
-    result = fetch_processed_transactions_from_batch(1, 11)
-    print(result)
