bf524d8d4546226bcae66809f27819f1173a573c
Chris Lacina <clacina@gmail.com>
2023-10-01 09:17:30 -0700

    Transaction expand with data !!!

--- spending-ui/src/components/processed-transactions/transaction_detail.component.jsx
+++ spending-ui/src/components/processed-transactions/transaction_detail.component.jsx
@@ -1,72 +1,35 @@
 import React, {useContext} from "react";
-import BootstrapTable from "react-bootstrap-table-next";
-import {Item, Menu, Separator, Submenu} from "react-contexify";
 import {StaticDataContext} from "../../contexts/static_data.context.jsx";
+import {ItemTable} from './transaction_detail.component.styles.jsx';
 
 
 const TransactionDetailComponent = ({row}) => {
     const {transactionDataDefinitions} = useContext(StaticDataContext);
-
-    console.log("Row: ", row);
     const dataDefinition = transactionDataDefinitions.filter((x, idx) => x.institution_id === row.institution_id);
-    const columns = [];
-    console.log("Dd: ", dataDefinition);
-    dataDefinition.forEach((x) => {
-        if(x.data_id) {
-            columns.push({
-                dataField: x.data_id,
-                text: x.column_name,
-                // headerStyle: (column, colindex) => {
-                //     return {display: 'inline-flex'};
-                // },
-                // style: {
-                //     backgroundColor: 'light-blue',
-                //     color: 'white'
-                // }
-            });
-        }
-    });
 
-    columns.push({dataField: 'keyid', text: '', isDummyField: true, hidden: true})
-    const processed_data = {}
-    var ndx = 0;
-    var colNdx = 0;
-    row.transaction.transaction_data.forEach((item) => {
-        if(dataDefinition[ndx].data_id) {
-            const colName = columns[colNdx].dataField;
-            console.log("col name: ", colName);
-            processed_data[colName] = item;
-            colNdx++;
-        }
-        ndx++;
-    })
-    console.log("new data: ", processed_data);
-
+    const tableDef = []
+    for(let i=0;i<dataDefinition.length;i++) {
+        const line = {}
+        line.title = dataDefinition[i].column_name;
+        line.value = row.transaction.transaction_data[i];
+        tableDef.push(line);
+    }
 
     return(
-        <>
-            <BootstrapTable
-                keyField='keyid'
-                data={[processed_data]}
-                columns={columns}
-
-            />
-            {/*<Menu id="context-menu" theme='dark'>*/}
-            {/*        <>*/}
-            {/*            <Item className="text-center">Header row</Item>*/}
-            {/*            <Separator/>*/}
-            {/*            {["Google", "Apple"].includes("Google") && (*/}
-            {/*                <Submenu label="Contact" arrow=">">*/}
-            {/*                    <Item>Phone</Item>*/}
-            {/*                    <Item>Email</Item>*/}
-            {/*                </Submenu>*/}
-            {/*            )}*/}
-            {/*            <Item disabled={true}>Add to Cart</Item>*/}
-            {/*        </>*/}
-            {/*    )*/}
-            {/*</Menu>*/}
-        </>
-    )
+        <ItemTable>
+            <thead>
+                <tr>
+                    <td>Field</td>
+                    <td>Value</td>
+                </tr>
+            </thead>
+            <tbody>
+                {tableDef.map((item) => {
+                    return(<tr><td>{item.title}</td><td>{item.value}</td></tr>);
+                })}
+            </tbody>
+        </ItemTable>
+    );
 }
 
 export default TransactionDetailComponent;
