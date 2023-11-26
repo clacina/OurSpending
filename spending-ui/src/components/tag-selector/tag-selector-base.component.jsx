8b2db577b88d88275506c8c3ec792da7cd340c4e
Chris Lacina <clacina@gmail.com>
2023-11-26 10:58:41 -0800

    Refactor of tag selectors

---
+++ spending-ui/src/components/tag-selector/tag-selector-base.component.jsx
@@ -0,0 +1,76 @@
+import {StylesConfig} from "react-select";
+import {ColourOption} from "./data.tsx";
+import chroma from "chroma-js";
+
+export const colourStyles: StylesConfig<ColourOption, true> = {
+    control: (styles) => ({ ...styles, backgroundColor: 'white' }),
+    option: (styles, { data, isDisabled, isFocused, isSelected }) => {
+        const color = chroma(data.color);
+        return {
+            ...styles,
+            backgroundColor: isDisabled
+                ? undefined
+                : isSelected
+                    ? data.color
+                    : isFocused
+                        ? color.alpha(0.1).css()
+                        : undefined,
+            color: isDisabled
+                ? '#ccc'
+                : isSelected
+                    ? chroma.contrast(color, 'white') > 2
+                        ? 'white'
+                        : 'black'
+                    : data.color,
+            cursor: isDisabled ? 'not-allowed' : 'default',
+
+            ':active': {
+                ...styles[':active'],
+                backgroundColor: !isDisabled
+                    ? isSelected
+                        ? data.color
+                        : color.alpha(0.3).css()
+                    : undefined,
+            },
+        };
+    },
+    multiValue: (styles, { data }) => {
+        const color = chroma(data.color);
+        return {
+            ...styles,
+            backgroundColor: color.alpha(0.1).css(),
+        };
+    },
+    multiValueLabel: (styles, { data }) => ({
+        ...styles,
+        color: data.color,
+    }),
+    multiValueRemove: (styles, { data }) => ({
+        ...styles,
+        color: data.color,
+        ':hover': {
+            backgroundColor: data.color,
+            color: 'white',
+        },
+    }),
+};
+
+export const BuildOptions = (tagsMap, entity) => {
+    const tagColourOptions = []
+    tagsMap.forEach((item) => {
+        const tagOption = {}
+        tagOption['value'] = item.id;
+        tagOption['label'] = item.value;
+        tagOption['color'] = item.color;
+        tagColourOptions.push(tagOption);
+    });
+
+    const assigned = []
+    entity.tags.forEach((tag) => {
+        assigned.push(tagColourOptions.find((item) => {
+            return (item['value'] === tag.id)
+        }))
+    })
+
+    return {tagColourOptions, assigned};
+}
