d25c2a36687d2562288da866b94dc443f41d53e9
Chris Lacina <clacina@gmail.com>
2023-12-30 19:10:06 -0800

    Tags page styling and connection to context

--- spending-ui/src/components/tags/tags.component.jsx
+++ spending-ui/src/components/tags/tags.component.jsx
@@ -1,8 +1,8 @@
-import React, {useContext} from "react";
+import React, {useContext, useEffect} from "react";
 import "react-contexify/dist/ReactContexify.css";
-import { Row } from "react-bootstrap";
 import {StaticDataContext} from "../../contexts/static_data.context";
 
+import './tags.component.styles.css';
 import TagFormComponent from "./tag-form.component.jsx";
 import TagsTableComponent from "./tags-table.component.jsx";
 
@@ -10,17 +10,13 @@ import TagsTableComponent from "./tags-table.component.jsx";
 const TagsComponent = () => {
     const {setSectionTitle} = useContext(StaticDataContext);
     setSectionTitle('Tags');
+
     return (
-        <div>
-            <Row>
-                <h1>Tags</h1>
-            </Row>
-            <Row>
-                <TagFormComponent/>
-            </Row>
-            <Row>
-                <TagsTableComponent />
-            </Row>
+        <div id='tagsPageContainer'>
+            <TagFormComponent/>
+            <hr/>
+            <p>Click any column to edit that value.</p>
+            <TagsTableComponent />
         </div>
     )
 }
