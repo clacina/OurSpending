17c82df937d2a9bf3f71b2506413688bff1b570b
Chris Lacina <clacina@gmail.com>
2024-02-26 13:26:53 -0800

    Wrapping up credit card UI

--- spending-ui/src/components/credit-cards/credit.card.component.jsx
+++ spending-ui/src/components/credit-cards/credit.card.component.jsx
@@ -1,14 +1,18 @@
 import React, {useContext, useEffect, useState} from "react";
 import {StaticDataContext} from "../../contexts/static_data.context";
 import BootstrapTable from "react-bootstrap-table-next";
-import TagSelectorCategoryComponent from "../widgets/tag-selector/tag-selector-category.component";
 import {nanoid} from "nanoid";
+import './credit.card.component.styles.css';
+
 
 const CreditCards = () => {
     const {setSectionTitle, creditCardInfo, creditCardData} = useContext(StaticDataContext);
     const [isLoaded, setIsLoaded] = useState(false);
     const [columns, setColumns] = useState([]);
     const [creditCardRecords, setCreditCardRecords] = useState([]);
+    const [firstHalfPayment, setFirstHalfPayment] = useState(0.0);
+    const [secondHalfPayment, setSecondHalfPayment] = useState(0.0);
+    const [totalBalance, setTotalBalance] = useState(0.0);
 
     useEffect(() => {
         console.log("Start");
@@ -28,21 +32,41 @@ const CreditCards = () => {
     const buildCreditRecords = () => {
         let cardDetails = []
         creditCardInfo.forEach((item) => {
-            console.log("ccd: ", creditCardData);
             // Get latest balance record
             const card_data = creditCardData.filter((info) => {
                 return(info.card_id === item.id);
             });
-            console.log("Card Data: ", card_data);
             const recentRecord = card_data.sort(compareValues)[0]
-            console.log("recent: ", recentRecord);
             item.balance = recentRecord.balance;
             item.minimum_payment = recentRecord.minimum_payment;
             item.balance_date = recentRecord.balance_date;
             cardDetails.push(item);
         });
-        console.log("Result Set: ", cardDetails);
+
+        let fh = 0.0;
+        let sh = 0.0;
+        let tb = 0.0;
+        cardDetails.forEach((item) => {
+            if (item.due_date < 15) {
+                fh = fh + item.minimum_payment;
+            } else {
+                sh = sh + item.minimum_payment;
+            }
+            tb = tb + item.balance;
+        })
+        setFirstHalfPayment('$'+Intl.NumberFormat().format(fh));
+        setSecondHalfPayment('$'+Intl.NumberFormat().format(sh));
+        setTotalBalance('$'+Intl.NumberFormat().format(tb));
+
         return(cardDetails);
+    }
+
+    const amountColumnFormatter = (cell, row, rowIndex, formatExtraData) => {
+        return('$'+Intl.NumberFormat().format(cell));
+    }
+
+    const percentColumnFormatter = (cell, row, rowIndex, formatExtraData) => {
+        return((Math.round(cell * 100) / 100).toFixed(2) + '%');
     }
 
     const generateColumns = () => {
@@ -65,7 +89,7 @@ const CreditCards = () => {
 
         cols.push({
             dataField: 'balance',
-            text: 'Balance',
+            text: 'Balance (We Owe)',
             align: 'right',
             sort: true,
             editable: false,
@@ -73,8 +97,25 @@ const CreditCards = () => {
                 backgroundColor: headerBackgroundColor,
                 color: 'white'
             },
+            formatter: amountColumnFormatter,
             headerAttrs: {
-                width: '100px',
+                width: '160px',
+            }
+        });
+
+        cols.push({
+            dataField: 'credit_limit',
+            text: 'Credit Limit',
+            align: 'right',
+            sort: true,
+            editable: false,
+            headerStyle: {
+                backgroundColor: headerBackgroundColor,
+                color: 'white'
+            },
+            formatter: amountColumnFormatter,
+            headerAttrs: {
+                width: '140px',
             }
         });
 
@@ -89,7 +130,7 @@ const CreditCards = () => {
                 color: 'white'
             },
             headerAttrs: {
-                width: '100px',
+                width: '140px',
             }
         });
 
@@ -101,8 +142,9 @@ const CreditCards = () => {
             editable: false,
             headerStyle: {
                 backgroundColor: headerBackgroundColor,
-                color: 'white'
+                color: 'white',
             },
+            formatter: percentColumnFormatter,
             headerAttrs: {
                 width: '100px',
             }
@@ -118,6 +160,7 @@ const CreditCards = () => {
                 backgroundColor: headerBackgroundColor,
                 color: 'white'
             },
+            formatter: percentColumnFormatter,
             headerAttrs: {
                 width: '100px',
             }
@@ -148,6 +191,7 @@ const CreditCards = () => {
                 backgroundColor: headerBackgroundColor,
                 color: 'white'
             },
+            formatter: amountColumnFormatter,
             headerAttrs: {
                 width: '100px',
             }
@@ -158,23 +202,20 @@ const CreditCards = () => {
     }
 
     //-------------------- Event Handlers ---------------------------------
-    const amountColumnFormatter = (cell, row, rowIndex, formatExtraData) => {
-        return((Math.round(cell * 100) / 100).toFixed(2));
-    }
-
 
     if(isLoaded) {
-        console.log("Columns: ", columns);
-        console.log("Data: ", creditCardInfo);
         return (
-            <div>
-                <table>
-                    <BootstrapTable
-                        keyField='id'
-                        data={creditCardRecords}
-                        columns={columns}
-                    />
-                </table>
+            <div id='creditCardUIContainer'>
+                <BootstrapTable
+                    keyField='id'
+                    data={creditCardRecords}
+                    columns={columns}
+                />
+                <div id='creditCardSummaryContainer'>
+                    <p>Minimum Payment First of the Month: {firstHalfPayment}</p>
+                    <p>Minimum Payment Second Paycheck: {secondHalfPayment}</p>
+                    <p>Total Owed Balance: {totalBalance}</p>
+                </div>
             </div>
         );
     }
